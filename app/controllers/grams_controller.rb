581074b9183107e71ba372ed37e53efea668d292
Jim X. Lin <miljinx@gmail.com>
2017-02-08 14:15:51 -0800

    Add edit and update gram functionality

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -6,7 +6,7 @@ class GramsController < ApplicationController
 
   def show
     @gram = Gram.find_by_id(params[:id])
-    render plain: 'Not Found', status: :not_found if @gram.blank?
+    render_not_found if @gram.blank?
   end
 
   def new
@@ -22,9 +22,31 @@ class GramsController < ApplicationController
     end
   end
 
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    render_not_found if @gram.blank?
+  end
+
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+
+    @gram.update_attributes(gram_params)
+
+    if @gram.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def gram_params
     params.require(:gram).permit(:message)
+  end
+
+  def render_not_found
+    render plain: 'Not Found', status: :not_found
   end
 end
--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,6 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
+  describe "grams#update action" do
+    it "should allow users to successfully update grams" do
+      gram = FactoryGirl.create(:gram, message: "Initial value")
+      patch :update, params: { id: gram.id, gram: { message: 'New Value' } }
+      expect(response).to redirect_to root_path
+      gram.reload
+      expect(gram.message).to eq "New Value"
+    end
+
+    it "should return a 404 if the gram is not found" do
+      patch :update, params: { id: 'NOTANID', gram: { message: 'New Value' } }
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it "should properly deal with validation errors" do
+      gram = FactoryGirl.create(:gram, message: "Initial Value")
+      patch :update, params: { id: gram.id, gram: { message: '' } }
+      expect(response).to have_http_status(:unprocessable_entity)
+      gram.reload
+      expect(gram.message).to eq "Initial Value"
+    end
+  end
+
+  describe "grams#edit action" do
+    it "should successfully show the edit form if the gram is found" do
+      gram = FactoryGirl.create(:gram)
+      get :edit, params: { id: gram.id }
+      expect(response).to have_http_status(:success)
+    end
+
+    it "should return a 404 if the gram is not found" do
+      get :show, params: { id: 'NOTANID' }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
   describe "grams#show action" do
     it "should successfully show the page if the gram is found" do
       gram = FactoryGirl.create(:gram)
