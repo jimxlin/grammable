5fe3ff09f77e3cb7cb68df30e6398ee5ca71bdd6
Jim X. Lin <miljinx@gmail.com>
2017-02-12 17:18:31 -0800

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+Gram.delete_all
+Comment.delete_all
+
+############################# Users ####################################
+
+16.times do |n|
+  User.create!(
+    email:    "foobar-#{n}@foobar.com",
+    password: "foobar"
+  )
+end
+
+user_ids = []
+User.find_each { |user| user_ids << user.id }
+
+############################# Grams ####################################
+
+8.times do |n|
+  message = Faker::Hipster.paragraph(4, false, 1)
+  img_path = "db/seed_data/lenna.jpg"
+  user_id = user_ids.sample
+
+  Gram.create!(
+    message: message,
+    picture: Rails.root.join(img_path).open,
+    user_id: user_id
+  )
+end
+
+gram_ids = []
+Gram.find_each { |gram| gram_ids << gram.id }
+
+############################ Comments ##################################
+
+24.times do
+  message = Faker::Hipster.paragraph(2, false, 1)
+  gram_id = gram_ids.sample
+  user_id = user_ids.sample
+
+  Comment.create!(
+    message: message,
+    gram_id: gram_id,
+    user_id: user_id
+  )
+end
